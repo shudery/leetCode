3c474dc77c5a67d35f2599b0babc1970523c6eda
daguo <shudery@foxmail.com>
2016-11-04 10:37:39 +0800

    update

--- test.js
+++ test.js
@@ -7,16 +7,11 @@ var num = process.env.num;
 
 var path = './problems';
 var problems = fs.readdirSync(path);
+
 console.log('start fns ac test:')
+
 if (num) {
     //测试指定目录
-    oneTest(problems, num);
-} else {
-    //全部测试
-    allTest(problems);
-};
-
-function oneTest(problems, num) {
     let reg = new RegExp(num);
     let problem = '';
     //匹配相应题目
@@ -25,7 +20,16 @@ function oneTest(problems, num) {
             problem = val;
         }
     })
-    let fns = require('./problems/' + problem + '/index.js');
+    test(problem);
+} else {
+    //全部测试
+    problems.forEach((problem) => {
+    	test(problem)
+    });
+};
+
+function test(problem){
+	let fns = require('./problems/' + problem + '/index.js');
     let demo = require('./problems/' + problem + '/test.js');
     //将单个函数接口 转换为数组格式
     if (Object.prototype.toString.call(fns) !== '[object Array]') {
@@ -44,34 +48,5 @@ function oneTest(problems, num) {
                 });
             })
         })
-    });
-
-    //单测将开启函数性能测试
-    // speedTest(problem,fns,demo)
-};
-
-function allTest(problems) {
-    problems.forEach((problem) => {
-
-        let fns = require('./problems/' + problem + '/index.js');
-        let demo = require('./problems/' + problem + '/test.js');
-
-        //将单个函数接口 转换为数组格式
-        if (Object.prototype.toString.call(fns) !== '[object Array]') {
-            let fn = fns;
-            fns = [];
-            fns.push(fn)
-        }
-        describe(("test-problem:" + problem).yellow, function() {
-            //每个函数测试
-            fns.forEach((fn, index) => {
-                //每个输入例子测试
-                demo.forEach((val, i) => {
-                    it("function:" + fn.name + "  demo-" + i, function() {
-                        expect(fn.apply(null, val.input)).to.deep.equal(val.output);
-                    });
-                })
-            })
-        });
     });
 }
