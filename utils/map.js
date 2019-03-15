34534dd0e5131ae8a3c26d65d6ea8ae8c93a4a80
linxiuda <shudery@foxmail.com>
2019-03-15 09:33:19 +0800

    add mapTree tils

--- utils/map.js
+++ utils/map.js
@@ -17,27 +17,47 @@ function mapLinks(arr) {
  * 数组 -> 二叉树
  * [1,"2",null,null,3,4,5]
  *
- * ->
+ * -> 先按层级转为二维数组
  * [
  *  [1]
  *  ["2",null]
  *  [null,3,null,null]
  *  [null,null,4,5,null,null,null,null]
  * ]
+ * -> 再转为树结构
  *      1
- *    2
+ *   "2"
  *      3
  *     4 5
  */
 function mapTree(array) {
-  const result = [];
-  let arr = array;
-  let size = 1;
-  while (arr.length) {
-    let res = [];
-    while (size--) {
-      res.push(arr.shift());
-    }
+  // 一维数组按层级转为二维数组
+  let arr = [array.shift()];
+  const result = [arr.map(v => ({ val: v }))];
+  while (array.length) {
+    let nextArr = [];
+    arr.forEach(v => {
+      if (v !== null) nextArr = [...nextArr, ...array.splice(0, 2)];
+      else nextArr = [...nextArr, null, null];
+    });
+    arr = nextArr;
+    result.push(arr.map(v => ({ val: v })));
   }
+  // 转换二维数组为树结构
+  let d;
+  const root = result[0][0];
+  // 取当前层数组，与下一层级连接起来
+  while ((d = result.shift()) && result[0]) {
+    let i = 0;
+    d.forEach(v => {
+      // null结点没有子节点，但数组中仍存有占位符，i需要累加2
+      if (v.val === null) i += 2;
+      else {
+        v.left = result[0][i++];
+        v.right = result[0][i++];
+      }
+    });
+  }
+  return root;
 }
-module.exports = { mapLinks };
+module.exports = { mapLinks, mapTree };
