38fadc6208c879195d2cca808a9f8e74f84fd00a
linxiuda <shudery@foxmail.com>
2019-03-28 11:25:09 +0800

    test perf of foreach and normal for circulate

--- problems/999-test/test.js
+++ problems/999-test/test.js
@@ -1,7 +1,9 @@
-const arr = new Array(10000).fill(0);
+const arr10000 = new Array(10000).fill(0);
+const arr100 = new Array(100).fill(0);
+const arr10 = new Array(10).fill(0);
 module.exports = [
   {
-    input: [arr],
+    input: [arr100],
     output: undefined
   }
 ];
